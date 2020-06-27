415a529e685c5c44d55777a4f45a625eee2c2d58
Sergey Gurman <sgurman753@gmail.com>
2020-06-27 19:07:10 -0400

    added a why to get the users input for amout of water, milk, and beans

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -2,6 +2,11 @@ WATER = 200  # ml
 MILK = 50  # ml
 COFFEE_BEANS = 15  # g
 
+message = "Write how many {} of {} the coffee machine has: "
+water = int(input(message.format("ml", "water")))
+milk = int(input(message.format("ml", "milk")))
+coffee_beans = int(input(message.format("g", "coffee beans")))
+
 cups = int(input("Write how many cups of coffee you will need: "))
 print(f"For {cups} cups of coffee you will need:\n"
       f"{cups * WATER} ml of water\n"
