cf80abe578c634859f28da09fc373cfed706efa2
Sergey Gurman <sgurman753@gmail.com>
2020-06-28 17:29:57 -0400

    made a function that fill items in the shop

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -1,38 +1,51 @@
 # The types of drinks that the coffee shop has.
 drinks = {"espresso": {"cost": 4.0,
-                       "recipe": {"water": [250, "ml"],
-                                  "coffee beans": [16, "g"]}},
+                       "recipe": {"water": 250,
+                                  "coffee beans": 16}},
           "latte": {"cost": 7.0,
-                    "recipe": {"water": [350, "ml"],
-                               "milk": [75, "ml"],
-                               "coffee beans": [20, "g"]}},
+                    "recipe": {"water": 350,
+                               "milk": 75,
+                               "coffee beans": 20}},
           "cappuccino": {"cost": 6.0,
-                         "recipe": {"water": [200, "ml"],
-                                    "milk": [100, "ml"],
-                                    "coffee beans": [12, "g"]}}
+                         "recipe": {"water": 200,
+                                    "milk": 100,
+                                    "coffee beans": 12}}
           }
 # What the store has in stock.
-stock = {"water": 400,
-         "milk": 540,
-         "coffee beans": 120,
-         "disposable cups": 9,
-         "money": 550}
+stock = {"water": [400, "ml"],
+         "milk": [540, "ml"],
+         "coffee beans": [120, "grams"],
+         "disposable cups": [9, None],
+         "money": [550, None]}
 
 
 def show_supplies():
     print("The coffee machine has: ")
-    for item in stock:
-        print(f"{stock[item]} of {item}")
+    for key, value in stock.items():
+        print(f"{value[0]} of {key}")
 
 
-message = "Write how many {} of {} the coffee machine has: "
-# The amount of ingredient that are stocked
-amount_water = int(input(message.format("ml", "water")))
-amount_milk = int(input(message.format("ml", "milk")))
-amount_beans = int(input(message.format("g", "coffee beans")))
+def buy_drink():
+    pass
+
+
+def fill_supplies():
+    message = "Write how many {} of {} do you want to add: "
+    for key, value in stock.items():
+        if value[1]:
+            stock[key][0] = value[0] + int(input(message.format(value[1], key)))
+        elif key == "disposable cups":
+            stock[key][0] = value[0] + int(input(message.format(key, "coffee")))
+
+
+def take_money():
+    pass
+
+
+show_supplies()
+fill_supplies()
 amount_ingredients = [amount_water // WATER, amount_beans // COFFEE_BEANS, amount_milk // MILK]
 many_cups_made = min(amount_ingredients)
-
 cups = int(input("Write how many cups of coffee you will need: "))
 require_water = cups * WATER
 require_milk = cups * MILK
