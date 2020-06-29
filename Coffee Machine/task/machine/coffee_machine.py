1ceb852005dada366a3a9a16b28b1b4d4b63b107
Sergey Gurman <sgurman753@gmail.com>
2020-06-28 23:51:55 -0400

    removed old code

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -59,18 +59,4 @@ show_supplies()
 buy_drink()
 show_supplies()
 fill_supplies()
-amount_ingredients = [amount_water // WATER, amount_beans // COFFEE_BEANS, amount_milk // MILK]
-many_cups_made = min(amount_ingredients)
-cups = int(input("Write how many cups of coffee you will need: "))
-require_water = cups * WATER
-require_milk = cups * MILK
-require_beans = cups * COFFEE_BEANS
 
-if amount_water >= require_water and amount_milk >= require_milk and amount_beans >= require_beans:
-    enough = "Yes, I can make that amount of coffee"
-    if many_cups_made > cups:
-        print(enough + f"(and even {many_cups_made - cups} more than that)")
-    else:
-        print(enough)
-else:
-    print(f"No, I can make only {many_cups_made} cups of coffee")
